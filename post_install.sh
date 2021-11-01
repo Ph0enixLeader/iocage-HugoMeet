db0b5e513a11ffd60e0f4121da48553e34953121
Ph0enixLeader <77356102+Ph0enixLeader@users.noreply.github.com>
2021-11-01 16:09:25 +0100

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -4,34 +4,34 @@ mkdir -p /usr/local/www
 mkdir -p /usr/local/etc/rc.d		
 
 # Clone source from official HugoMeet repository
-#cd /usr/local/www
-#git clone git://github.com/Ph0enixLeader/HugoMeetDev --quiet --depth 1 HugoMeet
-#if [ $? -ne 0 ] ; then
-#  echo "ERROR: Failed to get source from HugoMeet Repository"
-#  exit 1
-#fi
+cd /usr/local/www
+git clone git://github.com/Ph0enixLeader/HugoMeetDev --quiet --depth 1 HugoMeet
+if [ $? -ne 0 ] ; then
+  echo "ERROR: Failed to get source from HugoMeet Repository"
+  exit 1
+fi
 
 # Create user mcserver
-#pw useradd -n hugomeetserver -u 199 -G www -d /nonexistent -s /usr/local/bin/bash -h 0 <<EOF
-#hugomeetserver
-#EOF
+pw useradd -n hugomeetserver -u 199 -G www -d /nonexistent -s /usr/local/bin/bash -h 0 <<EOF
+hugomeetserver
+EOF
 
 # Enable the service
-#chmod +x *.sh
-#chmod +x /usr/local/etc/rc.d/HugoMeet
-#sysrc -f /etc/rc.conf HugoMeet_enable="YES"
+chmod +x *.sh
+chmod +x /usr/local/etc/rc.d/HugoMeet
+sysrc -f /etc/rc.conf HugoMeet_enable="YES"
 
 # Start the service
-#service HugoMeet start
+service HugoMeet start
 
 #service HugoMeet status >/dev/null 2>&1
-#if [ $? -ne 0 ] ; then
-#  echo "ERROR: Failed to start service"
-#  exit 1
-#fi
+if [ $? -ne 0 ] ; then
+  echo "ERROR: Failed to start service"
+  exit 1
+fi
 
-#echo
-#cat <<EOF > /root/PLUGIN_INFO
+echo
+cat <<EOF > /root/PLUGIN_INFO
 #---------------------------------------------------------------------#
 # Getting started with the HugoMeet plugin
 #---------------------------------------------------------------------#
@@ -39,6 +39,6 @@ mkdir -p /usr/local/etc/rc.d
 #HugoMeet is a server front-end to ease talk video.
 #For more information, see https://github.com/hcabel/HugoMeet
 
-#EOF
+EOF
 
-#cat /root/PLUGIN_INFO
+cat /root/PLUGIN_INFO
